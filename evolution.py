b57bd4c20fd8c5ff55ca944af2f4e433d9b40152
Tobias Moxter <tobiasmoxter@Tobiass-MBP.home>
2023-04-22 21:20:06 +0200

    Initial commit: nominally encoded representation of tree structures intorch for multiple genetic programming approaches.

---
+++ evolution.py
@@ -0,0 +1,214 @@
+import time
+import torch
+import torch.nn as nn
+from joblib.parallel import Parallel, delayed
+import pandas as pd
+from copy import deepcopy
+from framework import Framework
+#import cProfile as profiler
+# --> Remove parallization for insightful profiling
+
+
+class Evolution:
+    """
+    Simple evolutionary algorithm similar to Genepro that uses the encoding to make small local variations
+    in the form of brief random walks in the endoced space. The new variations yield the offspring.
+    Selection step is tournament-selection from https://github.com/marcovirgolin/genepro/blob/main/genepro/selection.py.
+
+    Parameters
+    ------
+    max_evals :int: maximum allowed number of evaluations,
+    max_gens :int: maximum allowed number of generations,
+    max_time :float: maximum time to run in seconds,
+    verbose :bool: per generation statements,
+    log :object: torchvision SummaryWriter or None
+
+    Attributes
+    -------
+    best_of_gen :list: list of best solutions of each generation, pos [-1] for best overall
+
+    """
+    
+    def __init__(self,
+        framework : Framework, model : nn.Module,
+        data_x, data_y,
+        tournament_size : int = -1,
+        population_size : int = 100,
+        max_evals : int = None, max_gens : int = 100, max_time : int = None,
+        verbose : bool = False, writer : object = None,
+        ) -> None:
+
+        self.framework, self.model = framework, model
+        self.data_x, self.data_y = data_x, data_y
+        self.population_size = population_size
+        self.tournament_size = tournament_size
+        self.max_evals, self.max_gens = max_evals, max_gens
+        self.max_time = max_time
+        self.verbose, self.log = verbose, writer
+
+        self.best_t = list()
+        self.best_f = list()
+        self.visited = dict()
+        self.time_at_gen, self.eval_at_gen = [], []
+        self.save_data = []
+        self.n_rejected = [0, 0]
+        
+    def evolve(self, proceed : bool = False) -> None:
+        """Initialize new generation and evolve until termination criteria are met.
+        
+        Parameters
+        ------
+        
+        Returns
+        ------
+        None
+        """
+        if not proceed:
+            self.start_time = time.time()
+            self.num_gens, self.num_evals = 0, 0
+            self._initialize_population()
+        
+        # --- generational loop ---
+        while not self._must_terminate():
+            # --- --- perform one generation --- ---
+            self._perform_generation()
+            # --- --- logging & printing --- ---
+            self.save_data.append(
+                {
+                    "n_gen":self.num_gens, "f":self.fitnesses.max().item(),
+                    "fitnesses":self.fitnesses, "n_eval": self.num_evals,
+                    "t":time.time() - self.start_time,
+                    "tree":self.best_t[-1],
+                    #"frac_reject": self.n_rejected[0]/self.n_rejected[1],
+                   # "df_gen":torch.tensor(self.df_gen),
+                    #"d2_gen":torch.tensor(self.d2_gen),
+                    #"df_median":torch.tensor(self.df_gen)
+                }
+            )
+            if self.log:
+                self.log.add_scalar("Best v. nGen", 
+                    self.best_f[-1], self.num_gens)
+                self.log.add_scalar("Pop Fitness v. nGen",
+                    self.fitnesses.sum().item(), self.num_gens)
+            if self.verbose:
+                print("Gen: {}, evals: {}, best of gen fitness: {:.5f}, gen fitness: {:.3f}"\
+                    .format(self.num_gens, self.num_evals, self.best_f[-1],
+                    self.fitnesses.sum().item())
+                )
+    
+        self.results = pd.DataFrame.from_dict(self.save_data)
+    
+    def _initialize_population(self):
+        """Initialize the start population and evaluate its fitness"""
+        self.population = self.framework.new_population(self.population_size)
+        self.semantics = self.framework.evaluate(self.population)
+        self.fitnesses = self.framework.fitness(self.semantics, self.data_y)
+
+    def _perform_generation(self) -> None:
+        """Evolve one generation."""
+
+        self.df_gen, self.d2_gen, self.n_rejected = [], [], [0,0]
+
+        # --- variation --- 
+        #offspring = self.model.variation(self.population)
+        offspring = deepcopy(self.population)
+        offspring_semantics = self.framework.evaluate(offspring)
+        offspring_fitnesses = self.framework.fitness(offspring_semantics,
+                                                     self.data_y)
+        # --- selection ---
+        if self.tournament_size == -1:
+            improved = offspring_fitnesses >= self.fitnesses
+            self.population[improved] = offspring[improved]
+            self.semantics[improved] = offspring_semantics[improved]
+            self.fitnesses[improved] = offspring_fitnesses[improved]
+        else:
+            win_p, win_o = self._tournament_selection(offspring_fitnesses,
+                                                      include_parents=True)
+            self.population = torch.cat(
+                (self.population[win_p], offspring[win_o])
+                )
+            self.fitnesses = torch.cat(
+                (self.fitnesses[win_p], offspring_fitnesses[win_o])
+                )
+            self.semantics = torch.cat(
+                (self.semantics[win_p], offspring_semantics[win_o])
+                )
+           
+        # --- update info ---
+        self.num_gens += 1
+        
+        elitist = torch.argmax(self.fitnesses)
+        self.best_t.append(deepcopy(self.population[elitist]))
+        self.best_f.append(self.fitnesses[elitist])
+        self.time_at_gen.append(time.time() - self.start_time)
+        self.eval_at_gen.append(self.num_evals)
+    
+    def _must_terminate(self) -> bool:
+        """Check if termination must occur."""
+
+        self.elapsed_time = time.time() - self.start_time
+        if self.max_time and self.elapsed_time >= self.max_time:
+            return True
+        elif self.max_evals and self.num_evals >= self.max_evals:
+            return True
+        elif self.max_gens and self.num_gens >= self.max_gens:
+            return True
+        
+        return False
+    
+    def _tournament_selection(self, offspring_fitnesses : torch.tensor,
+                              include_parents : bool = True):
+
+        if include_parents:
+            contestants = torch.cat((self.fitnesses, offspring_fitnesses))
+        else:
+            contestants = offspring_fitnesses
+
+        n_to_select = self.population_size
+        n = contestants.shape[0]
+        n_per_parse = n // self.tournament_size
+        n_parses = n_to_select // n_per_parse
+
+        # --- assert quantities are compatible ---
+        assert n / self.tournament_size == n_per_parse,\
+            "Number of contestants {} is not a multiple of tournament size {}"\
+                                            .format(n, self.tournament_size)
+        assert n_to_select / n_per_parse == n_parses
+        
+        winners = torch.zeros(n_to_select, requires_grad=False, dtype=int)
+        indices = torch.arange(0, n, 1)
+        for i in range(n_parses):
+            p = torch.randperm(n)
+            select = torch.argmax(
+                contestants[p].reshape((-1, self.tournament_size)),
+                axis=1) + torch.arange(0, n, self.tournament_size)
+            winners[i*n_per_parse:(i+1)*n_per_parse] = indices[p][select]
+        
+        if include_parents:
+            from_parents = winners[winners < self.population_size]
+            from_offspring = winners[winners >= self.population_size]
+            return  from_parents, from_offspring % self.population_size
+        else:
+            return torch.empty(0), winners
+       
+    def valid_move(self, tree : list):
+        """Check if this state has been visited before. Add state to hasmap if new.
+        
+        Parameters
+        --------
+        tree :list: to be validated
+        
+        Returns
+        -------
+        :bool: whether or not the state is new
+        """
+        self.n_rejected[1] +=1
+
+        enc = tuple(self.lookup[node.symb] for node in tree[1:])
+        if enc in self.visited:
+            self.n_rejected[0] +=1
+            return False
+        else:
+            # --- if new state, store fitness ---
+            self.visited[enc] = tree[0]
+            return True
---
+++ framework.py
@@ -0,0 +1,155 @@
+import torch
+from torch.nn import functional as F
+import multiprocessing
+from joblib import Parallel, delayed
+
+class Framework:
+
+    def __init__(self, x, nodes, n_leafnodes, max_depth) -> None:
+        
+        nodes = [torch.mul, torch.add, torch.div, torch.sub,
+                 
+                 lambda a, b : torch.sin(a), lambda a, b: torch.sin(b),
+                 lambda a, b : torch.cos(a), lambda a, b: torch.cos(b),
+                 lambda a, b : torch.exp(a), lambda a, b: torch.exp(b),
+                 lambda a, b : torch.log(a), lambda a, b: torch.log(b),
+
+                 lambda a, b : torch.tensor(5).repeat(x.shape[0]),
+                 lambda a, b : torch.tensor(3.141).repeat(x.shape[0])]
+        
+        nodes += [lambda a, b : x[:,i] for i in range(x.shape[1])]
+
+        atomic_expr = [
+                lambda a, b : a+"*"+b, lambda a, b: a+"+"+b,
+                lambda a, b: a+'/'+b, lambda a, b: a + "-" + b,
+                lambda a, b : "sin(%s)"%a, lambda a, b: "sin(%s)"%b,
+                lambda a, b :"cos(%s)"%a, lambda a, b: "cos(%s)"%b,
+                lambda a, b : "exp(%s)"%a, lambda a, b: "exp(%s)"%b,
+                lambda a, b : "log(%s)"%a, lambda a, b: "log(%s)"%b,
+
+                lambda a, b : "5",
+                lambda a, b : '\u03C0'
+        ]
+        atomic_expr += [lambda a, b : "x_%s"%i for i in range(x.shape[1])]
+
+        self.nodes = {key: val for key, val in enumerate(nodes)}
+        self.atomic_expr = {key: val for key, val in enumerate(atomic_expr)}
+        self.max_depth, self.xdata = max_depth, x
+        self.leaf_info = ((2**(max_depth+1)+1)//2, n_leafnodes)
+        self.treeshape = (2**(max_depth+1)-1, len(nodes))
+        self.n_workers = multiprocessing.cpu_count()
+
+    def new_population(self, population_size : int):
+        sample_intern = torch.randint(0, self.treeshape[1]-self.leaf_info[1],
+                (population_size, self.treeshape[0]-self.leaf_info[0]))
+        sample_leaf = torch.randint(self.treeshape[1]-self.leaf_info[1],
+                self.treeshape[1], (population_size, self.leaf_info[0]))
+        sample = torch.cat((sample_intern, sample_leaf), dim=1)
+        return self.syntactic_embedding(sample)
+
+    def new_subtree(self, max_depth : int):
+        treeshape = (2**(max_depth+1)-1, len(self.nodes))
+        sample = torch.randint(0, treeshape[1],(1, treeshape[0]))
+        return self.syntactic_embedding(sample)
+    
+    def _evaluate_atomic_functions(self, indices : torch.tensor, shape : tuple,
+                                  alphabet : dict, touched_genes : list = None):
+        
+        semantics = torch.zeros(indices.shape[0], shape[0], shape[1],
+                                requires_grad=False)
+        
+        # --- evaluate leaf nodes ---
+        for j in range(semantics.shape[0]):
+            for i in range(shape[0]-1, shape[0]//2-1, -1):
+                semantics[j, i, :] = alphabet[indices[j, i].item()]('','')[:]
+
+        # --- evaluate internal nodes ---
+            for i in range(shape[0]//2-1, -1, -1):
+                semantics[j, i, :] = alphabet[indices[j, i].item()](
+                    semantics[j, 2*(i+1)-1, :],semantics[j, 2*(i+1), :])
+                                                
+        return semantics
+    
+    @torch.no_grad()
+    def evaluate(self, population : torch.tensor):
+        indices = torch.argmax(population, dim=2)
+        semantics = torch.vstack(
+            Parallel(n_jobs=self.n_workers)(
+            delayed(self._evaluate_atomic_functions)(
+            indices[i::self.n_workers],
+            shape = (self.treeshape[0], self.xdata.shape[0]),
+            alphabet = self.nodes
+            )
+            for i in range(self.n_workers)
+        ))
+
+        return semantics
+    
+    def syntactic_embedding(self, x : torch.tensor):
+        if x.dim() == 2:
+            return F.one_hot(x, num_classes=self.treeshape[1])
+        if x.dim() == 3:
+            indices = torch.argmax(x, dim=2)
+            return F.one_hot(indices, num_classes=self.treeshape[1])
+
+    def _nodewise_semantic(self, current_semantics: torch.tensor,
+                        domains : torch.tensor):
+        
+        encoded = torch.zeros(self.treeshape[1], requires_grad=False)
+        case_leaf = int((domains[0] == None)) * (self.treeshape[1] - self.leaf_info[1])
+        for i in range(case_leaf, self.treeshape[1]):
+            compare = self.nodes[i](domains[0], domains[1])
+            encoded[i] = 1/(1+F.mse_loss(compare, current_semantics)*.2)
+
+        sort = encoded.argsort(descending=False)[-5:]
+        masked = torch.zeros_like(encoded, requires_grad=False)
+        #encoded = encoded / encoded[sort].sum()
+        #masked[sort] = F.softmax(encoded[sort], dim=0)
+        masked[sort] = encoded[sort] / encoded[sort].sum()
+        return masked
+
+    @torch.no_grad()
+    def semantic_embedding(self, semantics : torch.tensor):
+        
+        embedding = torch.zeros((semantics.shape[0], *self.treeshape),
+                                requires_grad=False)
+        for j in range(embedding.shape[0]):
+            for i in range(self.treeshape[0]-1, self.treeshape[0]//2-1, -1):
+                embedding[j, i, :] = self._nodewise_semantic(
+                    current_semantics = semantics[j, i],
+                    domains = (None, None)
+                    )
+            for i in range(self.treeshape[0]//2-1, -1, -1):
+                embedding[j, i, :] = self._nodewise_semantic(
+                    current_semantics = semantics[j, i],
+                    domains = (semantics[j, 2*(i+1)-1], semantics[j, 2*(i+1)])
+                    )
+        return embedding
+    
+    def fitness(self, semantics : torch.tensor, target : torch.tensor):
+
+        se = (semantics[:, 0, :] - target.repeat(semantics.shape[0], 1))**2
+        return se.mean(dim = 1)
+
+    def as_tree(self):
+        raise NotImplementedError
+
+    def as_expression(self, population : torch.tensor):
+        if population.dim() == 2:
+            # --- single solution passed ---
+            population = population.unsqueeze(0)
+        indices = torch.argmax(population, dim=2)
+        output = list()
+
+        # --- evaluate leaf nodes ---
+        for j in range(indices.shape[0]):
+            string = ['']*self.treeshape[0]
+            for i in range(self.treeshape[0]-1, self.treeshape[0]//2-1, -1):
+                string[i] = self.atomic_expr[indices[j, i].item()]('','')[:]
+
+            for i in range(self.treeshape[0]//2-1, -1, -1):
+                string[i] = self.atomic_expr[indices[j, i].item()](
+                    string[2*(i+1)-1], string[2*(i+1)])
+            output.append(string[0])
+        return output
+        
