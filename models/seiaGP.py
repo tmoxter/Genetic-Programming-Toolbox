7fdb239e934d5149f578bb9723cb825bbd7a1588
Tobias Moxter <tmoxter@users.noreply.github.com>
2023-04-24 15:05:12 +0200

    Variation model for semantic embedding using interval arithmetics added.

---
+++ models/seiaGP.py
@@ -0,0 +1,28 @@
+#-----------------------------------------------#
+# Semantic Embedding using Interval Arithmetics #
+# ----------------------------------------------#
+
+import torch
+import numpy as np
+import math
+import multiprocessing
+from joblib import Parallel, delayed
+
+from framework import Framework
+
+class SeiaGP:
+
+    def __init__(self, framework : Framework) -> None:
+        self.framework = framework
+        self.n_workes = multiprocessing.cpu_count()
+    
+    def variation(self, semantics : torch.tensor,
+                 step_size : float) -> torch.tensor:
+        
+        embedded_space = self.framework.semantic_embedding(semantics)
+        angle = torch.rand_like(embedded_space).flatten(1)
+        angle = angle.div(torch.norm(angle, p=2, dim=1, keepdim=True))
+        embedded_space += angle.reshape_as(embedded_space) * step_size
+        
+        return self.framework.syntactic_embedding(embedded_space)
+    
