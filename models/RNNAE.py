a06f303afcb9ae060282ed2beb7b3e983ef728d3
Tobias Moxter <tmoxter@users.noreply.github.com>
2023-04-26 16:51:03 +0200

    New model-informed variation based on multiheaded decoder output

--- models/RNNAE.py
+++ models/RNNAE.py
@@ -4,13 +4,13 @@ from copy import deepcopy
 
 class RecurrentAutoEncoder(nn.Module):
     
-    def __init__(self, input_size, hidden_size, num_layers, seq_len):
+    def __init__(self, framework, input_size, hidden_size, num_layers, seq_len):
                  
         super(RecurrentAutoEncoder, self).__init__()
         self.encoder = RNNEncoder(input_size, hidden_size, num_layers)
         self.decoder = RNNDecoder(input_size, hidden_size, num_layers)
-        self.seq_len = seq_len
-        self.num_layers = num_layers
+        self.seq_len, self.num_layers = seq_len, num_layers
+        self.framework = framework
 
     def forward(self, x, teacher_forcing = True):
         
@@ -19,6 +19,32 @@ class RecurrentAutoEncoder(nn.Module):
                                encoded, teacher_forcing)
 
         return decoded
+    
+    def variation(self,  population : torch.tensor, *args):
+
+        x = deepcopy(population).type(torch.float)
+        hidden_repr = self.encoder(x)
+        angle = torch.randn_like(hidden_repr)
+        norm = torch.sum(angle**2, dim=1)**.5
+        angle = torch.divide(angle.T, norm)
+        new_hidden_repr = torch.zeros_like(hidden_repr)
+        unchanged = torch.ones(hidden_repr.shape[0], dtype=bool)
+        step_size, increase = 0, 0.025
+        while unchanged.any(): 
+            new_hidden_repr[unchanged] = hidden_repr[unchanged]\
+                  + angle.T[unchanged]*step_size
+            step_size += increase
+            old = torch.argmax(self.decoder(x, hidden_repr, False), dim=2)
+            new = torch.argmax(self.decoder(x, new_hidden_repr, False), dim=2)
+            unchanged = (old == new).all(dim=1)
+            if step_size > 20:
+                break
+
+        new_reconstruction = self.decoder(x, new_hidden_repr, False)
+        old_reconstruction = self.decoder(x, hidden_repr, False)
+        x_new = x + (new_reconstruction - old_reconstruction)
+
+        return self.framework.syntactic_embedding(x_new)
 
 class RNNEncoder(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers):
--- models/denseAE.py
+++ models/denseAE.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 from copy import deepcopy
 
 class DenseAutoencoder(nn.Module):
-    def __init__(self, input_dim, hidden_dim, n_decoder_heads : int = 1):
+    def __init__(self, framework, input_dim, hidden_dim, n_decoder_heads : int = 1):
         super(DenseAutoencoder, self).__init__()
 
         self.encoder = nn.Sequential(
@@ -14,6 +14,7 @@ class DenseAutoencoder(nn.Module):
                          nn.LeakyReLU()) for _ in range(n_decoder_heads)]
         
         self.input_dim = input_dim
+        self.framework = framework
 
     def forward(self, x : torch.tensor):
         
@@ -23,35 +24,55 @@ class DenseAutoencoder(nn.Module):
         
         y = torch.cat([
             head(x_).unsqueeze(0) for head in self.decoder
-        ])
+            ])
 
         y = y.reshape(y.shape[0], *reshape)
         return y
     
-    def variation(self, x : torch.tensor):
+    def variation(self, population : torch.tensor, *args):
 
-        x = deepcopy(x)
+        x = deepcopy(population)
         reshape = x.shape
-        x = x.view(x.size(0), -1)
+
+        old_recons = self.forward(population)
+        population = population.expand(old_recons.shape[0], -1, -1, -1)
+        population = population.type(torch.float)
+        p = (1, 3, 0, 2) # -> reduction='none' applies cross_entropy along dim=1
+        sample_loss = nn.functional.cross_entropy(old_recons.permute(*p),
+                                population.permute(*p), reduction='none')
+        sample_loss = sample_loss.mean(dim = -1) # -> loss now of shape (n_samples, n_heads)
+        idx_head = torch.argmin(sample_loss, dim=1)
+        old_recons = old_recons.permute(1,0,2,3)
+        old_reconstruction = old_recons[torch.arange(idx_head.size(0)), idx_head]
+        old_seq = torch.argmax(old_reconstruction.reshape(reshape), dim=2)
+
+        x = x.view(x.size(0), -1).type(torch.float)
         hidden_repr = self.encoder(x)
+    
         angle = torch.randn_like(hidden_repr)
         norm = torch.sum(angle**2, dim=1)**.5
         angle = torch.divide(angle.T, norm)
         new_hidden_repr = torch.zeros_like(hidden_repr)
         unchanged = torch.ones(hidden_repr.shape[0], dtype=bool)
         step_size, increase = 0, 0.025
-        while unchanged.any(): 
+
+        while unchanged.any():
+
             new_hidden_repr[unchanged] = hidden_repr[unchanged] + angle.T[unchanged]*step_size
             step_size += increase
-            old = torch.argmax(self.decoder(hidden_repr).reshape(reshape), dim=2)
-            new = torch.argmax(self.decoder(new_hidden_repr).reshape(reshape), dim=2)
-            unchanged = (old == new).all(dim=1)
+
+            new_recon = torch.cat([
+                        head(new_hidden_repr).unsqueeze(0) for head in self.decoder
+                        ])
+            new_recon = new_recon.reshape(new_recon.shape[0], *reshape)
+            new_recon = new_recon.permute(1, 0, 2, 3)
+            new_reconstruction = new_recon[torch.arange(idx_head.size(0)), idx_head]
+            new_seq = torch.argmax(new_reconstruction.reshape(reshape), dim=2)
+
+            unchanged = (old_seq == new_seq).all(dim=1)
             if step_size > 20:
                 break
 
-        new_reconstruction = self.decoder(new_hidden_repr).reshape(reshape)
-        old_reconstruction = self.decoder(hidden_repr).reshape(reshape)
         x_new = x.reshape(reshape) + (new_reconstruction - old_reconstruction)
-        x_new = (x_new == torch.max(x_new, dim=2, keepdim=True).values).float()
         
-        return x_new
+        return self.framework.syntactic_embedding(x_new)
